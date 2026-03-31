0cd2865d0ecfdc2c5199f7b2f467e3716842f57d
aznos <aznosreal@gmail.com>
2026-03-31 18:22:50 -0400

    Init and tailwind install

---
+++ born-again/src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+    return <>
+        <h1 className="text-4xl text-primary">Truly, Truly, I say to you, unless one is born again he cannot see the kingdom of God.</h1>
+    </>
+}
+
+export default App
